0f7f967c08ba43d22131666be39a28b4f9f07b56
Noah <neglymph99@gmail.com>
2024-03-28 13:53:21 -0400

    added to 6th function

--- index.js
+++ index.js
@@ -43,6 +43,16 @@ const multiply = (int1, int2) => {
 
 // 6th function
 const add = (num1, num2) => {
-    return num1 + num2;
+    if (num1 === num2;) {
+     return ((num1 + num2) * (3))     
+    } else {
+        return ((num1 + num2));
+    }
+   
+
 }
-console.log(add(2, 4));
+console.log(add(2, 4));     //returns 6
+console.log(add(10, -5));   //returns 5
+console.log(add(3, 7.5));   //returns 10.5
+console.log(add(5, 5));    //returns 30 because the sum of 5 + 5 = 10 and 30 is triple that
+console.log(add(6, 6));     //returns 36 because the sum of 6 + 6 = 12 and 36 is triple that

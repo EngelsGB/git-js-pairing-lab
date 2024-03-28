370b724e54d6992aafc170b58a8108a866680125
Noah <neglymph99@gmail.com>
2024-03-28 13:12:03 -0400

    completed question 2

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //Code your solutions in this file
-const fiveToOneHundred = () => {
-    for (let i = 5; i <= 100; i++) {
-        console.log(i);
+// const fiveToOneHundred = () => {
+//    for (let i = 5; i <= 100; i++) {
+//        console.log(i);
+//    }
+//}
+//fiveToOneHundred(); 
+
+2. const multiplesOfThree = () => {
+    for (let i = 0; i <= 100; i++) {
+        if(i % 3 == 0 || i % 7 == 0
+        console.log(1);
     }
 }
-fiveToOneHundred();
